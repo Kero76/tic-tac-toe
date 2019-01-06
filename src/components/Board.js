2fc92834a8976844118c4b65af4755b94cf522c4
Nicolas GILLE <ngille@ftel.fr>
2019-01-06 15:48:28 +0100

    [Feat] - Add history feature.

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,41 +1,14 @@
 import React, { Component } from 'react';
 import Square from './Square';
-import Helper from './Helper';
 
 class Board extends Component {
-    //#region Constructors
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true,
-        }
-    }
-
-    //#endregion
-
     //#region Actions 
 
     renderSquare(i) {
         return <Square 
-            value={this.state.squares[i]} 
-            onClick={() => this.handleClick(i)}
+            value={this.props.squares[i]} 
+            onClick={() => this.props.onClick(i)}
         />;
-    }
-
-    handleClick(i) {
-        const squares = this.state.squares.slice();
-
-        if (Helper.computeWinner(this.state.squares) || squares[i])
-            return null;
-
-        squares[i] = (this.state.xIsNext) ? 'X' : 'O';
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
-        });
     }
 
     //#endregion
@@ -43,13 +16,8 @@ class Board extends Component {
     //#region Render
 
     render() {
-        const symbol = (this.state.xIsNext) ? 'X' : 'O';
-        const winner = Helper.computeWinner(this.state.squares);
-        const status = (winner) ? 'Winner: ' + winner : 'Next player: ' + symbol;
-
         return (
           <div>
-            <div className="status">{status}</div>
             <div className="board-row">
               {this.renderSquare(0)}
               {this.renderSquare(1)}
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,18 +1,88 @@
 import React, { Component } from 'react';
 import Board from './Board';
+import Helper from './Helper';
 
 class Game extends Component {
+    //#region Constructor
+
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null)
+            }],
+            xIsNext: true,
+            stepNumber: 0,
+        }
+    }
+
+    //#endregion
+
+    //#region Actions
+
+    handleClick(i) {
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+
+        if (Helper.computeWinner(squares))
+            return;
+
+        squares[i] = (this.state.xIsNext) ? 'X' : 'O';
+        this.setState({
+            history: history.concat([{
+                squares: squares,
+            }]),
+            stepNumber: history.length,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
+    goToStep(stepNumber) {
+        this.setState({
+            stepNumber : stepNumber,
+            xIsNext: (stepNumber % 2) === 0,
+        });
+    }
+
+    //#endregion
+
     //#region Render
 
     render() {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
+        const squares = current.squares.slice();
+        
+        const symbol = (this.state.xIsNext) ? 'X' : 'O';
+        const winner = Helper.computeWinner(squares);
+        const status = (winner) ? 'Winner: ' + winner : 'Next player: ' + symbol;
+
+        console.log(history);
+        const moves = history.map((step, move) => {
+            const desc = move 
+                ? 'Go to move #' + move 
+                : 'Go to start game';
+
+            return (
+                <li key={move}>
+                    <button onClick={() => this.goToStep(move)}>{desc}</button>
+                </li>
+            );
+        });
+
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board 
+                        squares={current.squares}
+                        onClick={i => this.handleClick(i)}
+                    />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
+                    <div>{ status }</div>
+                    <ol>{ moves }</ol>
                 </div>
             </div>
         );
