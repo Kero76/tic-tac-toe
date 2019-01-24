663780ac1580c920b7239f30d6e9a7e844b2679c
Nicolas GILLE <ngille@ftel.fr>
2019-01-24 14:21:50 +0100

    Updates

---
+++ precache-manifest.ddd0b8e646bb059e790eea7f5316da71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "6a0f25d46d3ad0b23248",
+    "url": "/tic-tac-toe/static/js/main.6a0f25d4.chunk.js"
+  },
+  {
+    "revision": "abd9e857442f04b96f9b",
+    "url": "/tic-tac-toe/static/js/1.abd9e857.chunk.js"
+  },
+  {
+    "revision": "6a0f25d46d3ad0b23248",
+    "url": "/tic-tac-toe/static/css/main.c164e90d.chunk.css"
+  },
+  {
+    "revision": "279770ccf91a0f1c177a4601c5fdc3bf",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
